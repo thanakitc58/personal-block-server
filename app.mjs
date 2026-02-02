aed19b09a79b1eda249d3eb91afecbe358120eb3
Pong <vx7vx8vx9@gmail.com>
2026-02-02 22:07:35 +0700

    refactor: Update database connection setup to use pg module directly and remove dotenv dependency. Adjusted connection string for direct use in the db utility module.

--- app.mjs
+++ app.mjs
@@ -38,7 +38,221 @@ app.post("/posts", async (req, res) => {
   return res.status(201).json({ message: "Created post successfully" });
 });
 
+//นักเขียนสามารถดูข้อมูลบทความอันเดียวได้
+app.get("/posts", async (req, res) => {
+  // ลอจิกในอ่านข้อมูลโพสต์ทั้งหมดในระบบ
+  try {
+    // 1) Access ข้อมูลใน Body จาก Request ด้วย req.body
+    const category = req.query.category || "";
+    const keyword = req.query.keyword || "";
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 6;
 
+    // 2) ทำให้แน่ใจว่า query parameter page และ limit จะมีค่าอย่างต่ำเป็น 1
+    const safePage = Math.max(1, page);
+    const safeLimit = Math.max(1, Math.min(100, limit));
+    const offset = (safePage - 1) * safeLimit;
+    // offset คือค่าที่ใช้ในการข้ามจำนวนข้อมูลบางส่วนตอน query ข้อมูลจาก database
+    // ถ้า page = 2 และ limit = 6 จะได้ offset = (2 - 1) * 6 = 6 หมายความว่าต้องข้ามแถวไป 6 แถวแรก และดึงแถวที่ 7-12 แทน
+
+    // 3) เขียน Query เพื่อ Insert ข้อมูลโพสต์ ด้วย Connection Pool
+    let query = `
+      SELECT posts.id, posts.image, categories.name AS category, posts.title, posts.description, posts.date, posts.content, statuses.status, posts.likes_count
+      FROM posts
+      INNER JOIN categories ON posts.category_id = categories.id
+      INNER JOIN statuses ON posts.status_id = statuses.id
+    `;
+    let values = [];
+
+    // 4) เขียน query จากเงื่อนไขของการใส่ query parameter category และ keyword
+    if (category && keyword) {
+      query += `
+        WHERE categories.name ILIKE $1 
+        AND (posts.title ILIKE $2 OR posts.description ILIKE $2 OR posts.content ILIKE $2)
+      `;
+      values = [`%${category}%`, `%${keyword}%`];
+    } else if (category) {
+      query += " WHERE categories.name ILIKE $1";
+      values = [`%${category}%`];
+    } else if (keyword) {
+      query += `
+        WHERE posts.title ILIKE $1 
+        OR posts.description ILIKE $1 
+        OR posts.content ILIKE $1
+      `;
+      values = [`%${keyword}%`];
+    }
+
+    // 5) เพิ่มการ odering ตามวันที่, limit และ offset
+    query += ` ORDER BY posts.date DESC LIMIT $${values.length + 1} OFFSET $${
+      values.length + 2
+    }`;
+
+    values.push(safeLimit, offset);
+
+    // 6) Execute the main query (ดึงข้อมูลของบทความ)
+    const result = await connectionPool.query(query, values);
+
+    // 7) สร้าง Query สำหรับนับจำนวนทั้งหมดตามเงื่อนไข พื่อใช้สำหรับ pagination metadata
+    let countQuery = `
+      SELECT COUNT(*)
+      FROM posts
+      INNER JOIN categories ON posts.category_id = categories.id
+      INNER JOIN statuses ON posts.status_id = statuses.id
+    `;
+    let countValues = values.slice(0, -2); // ลบค่า limit และ offset ออกจาก values
+
+    if (category && keyword) {
+      countQuery += `
+        WHERE categories.name ILIKE $1 
+        AND (posts.title ILIKE $2 OR posts.description ILIKE $2 OR posts.content ILIKE $2)
+      `;
+    } else if (category) {
+      countQuery += " WHERE categories.name ILIKE $1";
+    } else if (keyword) {
+      countQuery += `
+        WHERE posts.title ILIKE $1 
+        OR posts.description ILIKE $1 
+        OR posts.content ILIKE $1
+      `;
+    }
+
+    const countResult = await connectionPool.query(countQuery, countValues);
+    const totalPosts = parseInt(countResult.rows[0].count, 10);
+
+    // 8) สร้าง response พร้อมข้อมูลการแบ่งหน้า (pagination)
+    const results = {
+      totalPosts,
+      totalPages: Math.ceil(totalPosts / safeLimit),
+      currentPage: safePage,
+      limit: safeLimit,
+      posts: result.rows,
+    };
+    // เช็คว่ามีหน้าถัดไปหรือไม่
+    if (offset + safeLimit < totalPosts) {
+      results.nextPage = safePage + 1;
+    }
+    // เช็คว่ามีหน้าก่อนหน้าหรือไม่
+    if (offset > 0) {
+      results.previousPage = safePage - 1;
+    }
+    // 9) Return ตัว Response กลับไปหา Client ว่าสร้างสำเร็จ
+    return res.status(200).json(results);
+  } catch {
+    return res.status(500).json({
+      message: "Server could not read post because database issue",
+    });
+  }
+});
+
+//นักเขียนสามารถดูข้อมูลบทความอันเดียวได้
+app.get("/posts/:postId", async (req, res) => {
+  // ลอจิกในอ่านข้อมูลโพสต์ด้วย Id ในระบบ
+  // 1) Access ตัว Endpoint Parameter ด้วย req.params
+  const postIdFromClient = req.params.postId;
+
+  try {
+    // 2) เขียน Query เพื่ออ่านข้อมูลโพสต์ ด้วย Connection Pool
+    const results = await connectionPool.query(
+      `
+      SELECT posts.id, posts.image, categories.name AS category, posts.title, posts.description, posts.date, posts.content, statuses.status, posts.likes_count
+      FROM posts
+      INNER JOIN categories ON posts.category_id = categories.id
+      INNER JOIN statuses ON posts.status_id = statuses.id
+      WHERE posts.id = $1
+      `,
+      [postIdFromClient]
+    );
+
+    // เพิ่ม Conditional logic ว่าถ้าข้อมูลที่ได้กลับมาจากฐานข้อมูลเป็นค่า false (null / undefined)
+    if (!results.rows[0]) {
+      return res.status(404).json({
+        message: `Server could not find a requested post (post id: ${postIdFromClient})`,
+      });
+    }
+
+    // 3) Return ตัว Response กลับไปหา Client
+    return res.status(200).json({
+      data: results.rows[0],
+    });
+  } catch {
+    return res.status(500).json({
+      message: `Server could not read post because database issue`,
+    });
+  }
+});
+
+//นักเขียนสามารถแก้ไขข้อมูลบทความได้
+app.put("/posts/:postId", async (req, res) => {
+ 
+  // 1) Access ตัว Endpoint Parameter ด้วย req.params
+  // และข้อมูลโพสต์ที่ Client ส่งมาแก้ไขจาก Body ของ Request
+  const postIdFromClient = req.params.postId;
+  const updatedPost = { ...req.body, date: new Date() };
+
+  try {
+    // 2) เขียน Query เพื่อแก้ไขข้อมูลโพสต์ ด้วย Connection Pool
+    await connectionPool.query(
+      `
+        UPDATE posts
+        SET title = $2,
+            image = $3,
+            category_id = $4,
+            description = $5,
+            content = $6,
+            status_id = $7,
+            date = $8
+        WHERE id = $1
+      `,
+      [
+        postIdFromClient,
+        updatedPost.title,
+        updatedPost.image,
+        updatedPost.category_id,
+        updatedPost.description,
+        updatedPost.content,
+        updatedPost.status_id,
+        updatedPost.date,
+      ]
+    );
+
+    // 3) Return ตัว Response กลับไปหา Client
+    return res.status(200).json({
+      message: "Updated post successfully",
+    });
+  } catch {
+    // จัดการข้อผิดพลาดที่อาจเกิดขึ้นขณะ Query ฐานข้อมูล
+    return res.status(500).json({
+      message: `Server could not update post because database connection`,
+    });
+  }
+});
+
+//นักเขียนสามารถลบข้อมูลบทความได้
+app.delete("/posts/:postId", async (req, res) => {
+  // ลอจิกในการลบข้อมูลโพสต์ด้วย Id ในระบบ
+
+  // 1) Access ตัว Endpoint Parameter ด้วย req.params
+  const postIdFromClient = req.params.postId;
+
+  try {
+    // 2) เขียน Query เพื่อลบข้อมูลโพสต์ ด้วย Connection Pool
+    await connectionPool.query(
+      `DELETE FROM posts
+       WHERE id = $1`,
+      [postIdFromClient]
+    );
+
+    // 3) Return ตัว Response กลับไปหา Client
+    return res.status(200).json({
+      message: "Deleted post successfully",
+    });
+  } catch {
+    return res.status(500).json({
+      message: `Server could not delete post because database connection`,
+    });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server is running at ${port}`);
--- util/db.mjs
+++ util/db.mjs
@@ -1,11 +1,12 @@
-import pg from "pg";
-import dotenv from "dotenv";
-dotenv.config();
 
-const connectionString = process.env.CONNECTION_STRING;
+import * as pg from "pg";
+const { Pool } = pg.default;
 
-const connectionPool = new pg.Pool({
-  connectionString: connectionString,
+const connectionPool = new Pool({
+  // ตรงนี้ต้องเปลี่ยน connectionString เป็นของตัวเองด้วยนะ
+  connectionString:
+  "postgresql://postgres:0994672558Za@db.vzteohhdvktixczlyjan.supabase.co:5432/postgres",
+
 });
 
 export default connectionPool;
