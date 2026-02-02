59b858ca9d0093cb6f772ac640a0bea0cea17524
Pong <vx7vx8vx9@gmail.com>
2026-02-03 00:39:41 +0700

    refactor: Update database connection configuration for Supabase. Changed connection settings to use a pooler with specific host, user, and port, while commenting out the previous connection string.

--- util/db.mjs
+++ util/db.mjs
@@ -1,12 +1,19 @@
-
+import "dotenv/config";
 import * as pg from "pg";
+
 const { Pool } = pg.default;
 
-const connectionPool = new Pool({
-  
-  connectionString:
-  "postgresql://postgres:0994672558Za@db.vzteohhdvktixczlyjan.supabase.co:5432/postgres",
+// const connectionPool = new Pool({
+//   connectionString: "postgresql://postgres:0994672558Za@db.vzteohhdvktixczlyjan.supabase.co:5432/postgres",
+// });
 
-});
+const connectionPool = new Pool({
+  host: 'aws-1-ap-southeast-2.pooler.supabase.com',
+  user: 'postgres.vzteohhdvktixczlyjan',
+  password: '0994672558Za',
+  port: 6543,
+  database: 'postgres',
+  pool_mode: 'transaction',
+})
 
 export default connectionPool;
