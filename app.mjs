f326298ce79529f5b0214ab45b8646a8c3c6b0ce
Pong <vx7vx8vx9@gmail.com>
2026-02-02 19:23:53 +0700

    first commit

---
+++ app.mjs
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/profiles", (req, res) => {
+  return res.json({
+    data: {
+      name: "john",
+      age: 20,
+    },
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at ${port}`);
+});
