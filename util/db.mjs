9326cc14a6ff681b3c31808a1be54c132f7c6923
Pong <vx7vx8vx9@gmail.com>
2026-02-03 00:50:47 +0700

    refactor: Update database connection to use environment variable for connection string. Removed hardcoded connection settings and streamlined configuration for improved security and flexibility.

--- util/db.mjs
+++ util/db.mjs
@@ -3,17 +3,8 @@ import * as pg from "pg";
 
 const { Pool } = pg.default;
 
-// const connectionPool = new Pool({
-//   connectionString: "postgresql://postgres:0994672558Za@db.vzteohhdvktixczlyjan.supabase.co:5432/postgres",
-// });
-
 const connectionPool = new Pool({
-  host: 'aws-1-ap-southeast-2.pooler.supabase.com',
-  user: 'postgres.vzteohhdvktixczlyjan',
-  password: '0994672558Za',
-  port: 6543,
-  database: 'postgres',
-  pool_mode: 'transaction',
+  connectionString: process.env.CONNECTION_STRING,
 })
 
 export default connectionPool;
